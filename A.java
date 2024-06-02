aa56462e612ea3aee3c58116350f8cc7272a9f1a
Ishank Chopra <ishankchopranbd@gmail.com>
2024-06-02 17:36:24 +0530

    mkdnff

--- A.java
+++ A.java
@@ -3,5 +3,11 @@ class A {
 
     public static void main(String[] args) {
         System.out.println("my name is ishank chopra");
+
+        System.out.println("hello world");
+    
+
+);
+
     }
 }
